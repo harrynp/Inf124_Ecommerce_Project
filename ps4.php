da592ba821f7e656cb96a5be09a731c6d43df100
Harry <harrynp12@gmail.com>
2015-05-04 21:22:30 -0700

    Made 3ds product page

---
+++ ps4.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<!--INDEX.HTML-->
+
+<html>
+
+<head>
+  <title>Bros Game Shop</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <link rel="stylesheet" type="text/css" href="src/style.css" />
+  <script type="text/javascript" src="js/menu.js"></script>
+</head>
+
+<body onload="startRotation()">
+  <div>
+    <table>
+      <!--#include virtual="common/logo.php"-->
+      <tr>
+        <td>
+          <p>We will be bringing PS4 games to our lineup later!</p>
+        </td>
+      </tr>
+      <!--#include virtual="common/footer.php"-->
+    </table>
+  </div>
+</body>
+
+
+</html>
--- shop.php
+++ shop.php
@@ -8,13 +8,32 @@
   <title>Bros Game Shop</title>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
   <link rel="stylesheet" type="text/css" href="src/style.css" />
-
+  <script type="text/javascript" src="js/menu.js"></script>
 </head>
 
-<body>
+<body onload="startRotation()">
   <div>
     <table>
       <!--#include virtual="common/logo.php"-->
+      <tr>
+        <td align="center">
+          <a href="product1.php"><img src="images/product1.jpg" alt="Fire Emblem IF: Dark Night Kingdom" width="180px" height="180px"/></a>
+        </td>
+        <td align="center">
+          <a href="product2.php"><img src="images/product2.jpg" alt="Fire Emblem IF: White Night Kingdom" width="180px" height="180px"/></a>
+        </td>
+      </tr>
+      <tr>
+        <td align="center">
+          <a href="product1.php">Fire Emblem IF:Dark Night Kingdom</a>
+        </td>
+        <td align="center">
+          <a href="product2.php">Fire Emblem IF:White Night Kingdom</a>
+        </td>
+        <td>
+        </td>
+      </tr>
+      <!--#include virtual="common/footer.php"-->
     </table>
   </div>
 </body>
