d4de488527be91b96b616aa86b2c72aa22ec0d5c
amemon22 <amemon2@gmail.com>
2015-05-04 22:59:47 -0700

    Added email confirmation for the user

--- scripts/processFeedback.php
+++ scripts/processFeedback.php
@@ -14,4 +14,20 @@ $headerToBusiness =
   "From: $_POST[email] \r\n";
   mail("ahsanm@uci.edu", $_POST['subject'], $messageToBusiness, $headerToBusiness);
 
+//Construct confirmation message that will be e-mailed to the user
+$messageToClient =
+  "Dear ".$_POST['first_name']." ".$_POST['last_name']."\r\n" .
+  "The following message was recieved from you by Bro's Game Shop\r\n\r\n".
+  $messageToBusiness.
+  "-------------------------\r\nThank you for the feedback and you purchase.\r\n".
+  "The Bros Game Shop Team\r\n-------------------------";
+
+//Send the confirmation message to the client
+$headerToClient = "From: ahsanm@uci.edu\r\n";
+mail($_POST['email'], "Re: ".$_POST['subject'], $messageToClient, $headerToClient);
+
+//Confirmation for the user
+
+
+
 ?>
